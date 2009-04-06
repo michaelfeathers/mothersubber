10a70f74c3f20d33573fbeeed1de133a222128b7
Michael Feathers <mfeathers@acm.org>
2009-04-06 09:53:22 -0400

    Added some documentation for MotherSubber in the mothersubber_spec.rb file. Got rid of the head/tail thing in objectmatcher.rb

--- mothersubber_spec.rb
+++ mothersubber_spec.rb
@@ -1,4 +1,27 @@
-
+# MotherSubber is an experimental tool which allows you to replace 
+# objects deep within aggregates.  The intention is that you create 
+# aggregates in an ObjectMother and then use MotherSubber to replace 
+# bits and pieces for individual tests.
+#
+# It's rather easy to use MotherSubber. You simply include MotherSubber
+# in a class and then use the 'with' method to replace a piece.  In the 
+# simplest case, you supply 'with' with a list containing exactly one 
+# symbol.  MotherSubber walks through the object and all of its sub-objects
+# and it tries to find an instance variable with that name.  If it finds
+# exactly one, it sets that instance variable to point to the object you
+# supply as the other argument to 'with.'  If MotherSubber finds zero or 
+# more than one matches, it throws an exception. Since MotherSubber is 
+# intended to be used in tests, it is convenient to just throw an exception
+# when it's obvious that the internals of the object aren't what the user
+# expected them to be.
+#
+# If you've read this far you are probably thinking that it is rather 
+# ridiculous assume that all instance variables in an aggregate are
+# uniquely named.  What MotherSubber says (through it's API) is "wouldn't
+# it be nice if they were?" But, since reality often intrudes in awkward
+# ways, MotherSubber accepts a list of symbols which represent a partial
+# path to an object.  You can specify just as many leading instance 
+# variable names as you need to disambiguate the object you want to replace.
 
 require 'mothersubber'
 require 'obj'
