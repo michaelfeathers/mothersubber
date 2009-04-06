b73c471996685b76a1ff639827f5f226d21c374d
Michael Feathers <mfeathers@acm.org>
2009-04-06 09:12:41 -0400

    Adopted Lisp style head/tail for list traversal.

--- objectmatcher.rb
+++ objectmatcher.rb
@@ -3,13 +3,21 @@
 
 class ObjectMatcher
   
+  def self.head(list)
+    list.first
+  end
+  
+  def self.tail(list)
+    list[1..-1]
+  end
+  
   def self.matched_object(attr_symbols, object) 
     return nil if attr_symbols.length == 0
     
-    partial_match = object.instance_variable_get(attr_symbols.first)
+    partial_match = object.instance_variable_get(head(attr_symbols))
     return nil if not partial_match
     
-    attr_symbols.length == 1 ? object : matched_object(attr_symbols[1..-1], partial_match)
+    attr_symbols.length == 1 ? object : matched_object(tail(attr_symbols), partial_match)
   end
    
 end
